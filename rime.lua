b2f3cd7aee02117c8d41f270c195afeb92895852
abserari <abserari@gmail.com>
2023-03-09 16:18:07 +0800

    feat: # 增加了英文单词输入模式

--- rime.lua
+++ rime.lua
@@ -58,3 +58,16 @@ select_character_processor = require("select_character")
 -- date_translator: 动态日期时间输入
 -- 详见 `lua/date_translator.lua`
 date_translator = require("date_translator")
+
+-- from: https://github.com/shewer/rime-english  # 增加了英文单词输入模式 
+--  載入 function 
+--  將  { processor= { fini, init , func} ,translator={fini,init,func} .....}  載入全域
+load_module=require('tools/loadmodule')
+--  --load_module.load( 'module' , lua_component name, args ....)
+--    require 'lua/english/init.lua   ,
+--    lua_processor@english_processor ,  lua_segmentor@english_segmentor .....
+
+--    module args   'english_tw.txt'   字典檔    word\t[音標]; 翻譯  ......   \t
+load_module.load('english','english',"english_tw.txt") --  module , target_name , dict_file
+
+
